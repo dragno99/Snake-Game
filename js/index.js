0486e94585878420a8e5915ff610540a0ed214c5
Suryansh Kumar <76701875+dragno99@users.noreply.github.com>
2021-09-01 22:00:12 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 // Game constants and variable
 let inputDir = {x: 0 , y: 0};
 
-const foodSound = new Audio("https://github.com/dragno99/Snake-Game/blob/main/music/food.mp3");
-const gameOverSound = new Audio("https://github.com/dragno99/Snake-Game/blob/main/music/gameover.mp3");
-const moveSound = new Audio("https://github.com/dragno99/Snake-Game/blob/main/music/move.mp3");
-const musicSound = new Audio("https://github.com/dragno99/Snake-Game/blob/main/music/music.mp3");
+const foodSound = new Audio("../music/food.mp3");
+const gameOverSound = new Audio("../music/gameover.mp3");
+const moveSound = new Audio("../music/move.mp3");
+const musicSound = new Audio("../music/music.mp3");
+
 const speed = 5;
 let isMusicPlaying = 0;
 let score = 0;
